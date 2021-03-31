0f3eff9c481a64ec18ed9bfe7ca92f2084f8f166
Chris Benseler <cbenseler@gmail.com>
2021-03-31 11:12:18 -0300

    feat: modal sub component

--- pages/index.vue
+++ pages/index.vue
@@ -55,87 +55,8 @@
         ain’t getting them. <span> - Christopher McDougall</span>"
       </blockquote>
     </article>
-
-    <Modal v-show="isModalVisible" @close="closeModal">
-      <template v-slot:header>
-        <i class="fa fa-chalkboard-teacher"></i> Courses
-      </template>
-      <template v-slot:body>
-        <ul class="default-list">
-          <li>
-            AWS Serverless APIs & Apps - A Complete Introduction
-            <span class="badge">Udemy</span>
-          </li>
-          <li>Borland C++ Builder <span class="badge">Senai</span></li>
-          <li>Dart - Beginners Course <span class="badge">Udemy</span></li>
-          <li>
-            Desenvolvimento Ágil Ruby on Rails 3
-            <span class="badge">Caelum</span> Course numberRR-71
-          </li>
-          <li>
-            Developing Applications with GCP: App Deployment, Debugging, and
-            Performance <span class="badge">Coursera</span>
-          </li>
-          <li>
-            Developing Applications with GCP: Getting Started With Application
-            Development <span class="badge">Coursera</span>
-          </li>
-          <li>
-            Developing Applications with GCP: Securing and Integrating
-            Components of your Application <span class="badge">Coursera</span>
-          </li>
-          <li>Flutter - Advanced <span class="badge">Udemy</span></li>
-          <li>Flutter - Intermediate <span class="badge">Udemy</span></li>
-          <li>
-            Google Cloud Platform Fundamentals: Core Infrastructure
-            <span class="badge">Coursera</span>
-          </li>
-          <li>
-            GraphQL Apollo Server with Node.js, MongoDB
-            <span class="badge">Udemy</span>
-          </li>
-          <li>
-            How to design & develop REST microservices in Golang / Go
-            <span class="badge">Udemy</span>
-          </li>
-          <li>
-            JAVA e Orientação a Objetos <span class="badge">Caelum</span> Course
-            numberFJ-11
-          </li>
-          <li>
-            Mastering Visual Basic 6.0 Development
-            <span class="badge">KA Solutions</span>
-          </li>
-          <li>Modern Erlang for Beginners <span class="badge">Udemy</span></li>
-          <li>Mongo DB : All About MongoDB <span class="badge">Udemy</span></li>
-          <li>
-            NestJS Zero to Hero - Modern TypeScript Back-end Development
-            <span class="badge">Udemy</span>
-          </li>
-          <li>PHP Essentials <span class="badge">Senac</span></li>
-          <li>React Styled Components <span class="badge">Udemy</span></li>
-          <li>
-            Redis Bootcamp for Beginners: Get Hands On with Redis 5
-            <span class="badge">Udemy</span>
-          </li>
-          <li>
-            Securing Applications in Node.js <span class="badge">Udemy</span>
-          </li>
-          <li>
-            The Complete Flutter App Development Course for Android, iOS
-            <span class="badge">Udemy</span>
-          </li>
-          <li>
-            The Go Programming Language Tutorial - Volume 1
-            <span class="badge">Udemy</span>
-          </li>
-          <li>
-            Ultimate Kubernetes Fast-Track Beginner to Advanced
-            <span class="badge">Udemy</span>
-          </li>
-        </ul>
-      </template>
-    </Modal>
+    <CoursesModal v-bind:visible="isModalVisible" @close="closeModal" />
+    
   </section>
 </template>
 
@@ -143,13 +64,13 @@
 import SocialNetworks from "../components/SocialNetworks";
 import Skills from "../components/Skills";
 import ProfessionalExp from "../components/ProfessionalExp";
-import Modal from "../components/Modal.vue";
+import CoursesModal from "../components/modals/Courses.vue";
 export default {
   components: {
     SocialNetworks,
     Skills,
     ProfessionalExp,
-    Modal
+    CoursesModal
   },
   data() {
     return {
